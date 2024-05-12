d26a93f983df6418a30800d93117de10ca78a265
Lalitha-SR <145996677+Lalitha-SR@users.noreply.github.com>
2024-05-12 22:01:23 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modals = document.querySelectorAll('.modal');
+    modals.forEach(function(modal) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+}
